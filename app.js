8863b4c2007ca4149d34984d80a884e636e2c63d
bhoomi15296 <bhoomi15296@gmail.com>
2023-05-09 23:18:29 -0400

    Working with EJS - 09-May-2023

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine('handlebars', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout'})); 
-app.set('view engine', 'handlebars');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.render('404', { pageTitle: 'Page Not Found' });
+    res.render('404', { pageTitle: 'Page Not Found', path: '' });
 });
 
 app.listen(3000);
